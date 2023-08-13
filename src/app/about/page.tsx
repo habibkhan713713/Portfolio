033a4b0c1de529168a02cd632e752906ffc7bc9b
Habibullah Khan <habibkhan713713@gmail.com>
2023-08-13 18:51:27 +0500

    desktop version

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -19,19 +19,19 @@ const page = () => {
             Biography
           </h2>
           <p className="font-medium font-mono">
-            Hi, I'm <span className="font-bold">Habibullah Khan</span>, web
+            Hi, I&apos;m <span className="font-bold">Habibullah Khan</span>, web
             developer a passion for creating beautiful, functional, and
             user-centered digital experiences. With 0.4 years of experience in
             the field. I am always looking for new and innovative ways to bring
-            my clients' visions to life.
+            my clients&apos; visions to life.
           </p>
           <p className="font-medium my-4 font-mono">
             I believe that design is about more than just making things look
-            pretty – it's about solving problems and creating intuitive,
+            pretty – it&apos;s about solving problems and creating intuitive,
             enjoyable experiences for users.
           </p>
           <p className="font-medium font-mono">
-            Whether I'm working on a website, mobile app, or other digital
+            Whether I&apos;m working on a website, mobile app, or other digital
             product, I bring my commitment to design excellence and
             user-centered thinking to every project I work on. I look forward to
             the opportunity to bring my skills and passion to your next project.
--- src/app/components/AnimatedNumbers.tsx
+++ src/app/components/AnimatedNumbers.tsx
@@ -2,7 +2,7 @@
 import {useMotionValue,useInView,useSpring} from 'framer-motion'
 import { useEffect, useRef } from "react";
 
-export default  ({value}:any)=>{
+export default function({value}:any){
 const ref:any = useRef(null)
 const motionValue = useMotionValue(0)
 const springValue = useSpring(motionValue,{duration:3000})
