ab787c1ca02b824571891865bd870d23e7bbac71
Habibullah Khan <habibkhan713713@gmail.com>
2023-08-15 13:26:59 +0500

    Dark Theme

--- src/app/components/hooks/useThemeSwitcher.ts
+++ src/app/components/hooks/useThemeSwitcher.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export default function useThemeSwitcher() {
-  const preferDarkQuery = "(prefers-color-scheme:dark)";
+  const preferDarkQuery = "prefer-color-scheme:dark";
   const [mode, setMode] = useState("");
   useEffect(() => {
     const mediaQuery = window.matchMedia(preferDarkQuery);
@@ -9,41 +9,47 @@ export default function useThemeSwitcher() {
     function handleChange() {
       if (userPreference) {
         let check = userPreference === "dark" ? "dark" : "light";
-
         setMode(check);
         if (check === "dark") {
           document.documentElement.classList.add("dark");
+          document.documentElement.classList.remove("light");
+
         } else {
+          document.documentElement.classList.add("light");
           document.documentElement.classList.remove("dark");
         }
       } else {
         let check = mediaQuery.matches ? "dark" : "light";
-
         setMode(check);
         if (check === "dark") {
-          document.documentElement.classList.add("dark");
-        } else {
           document.documentElement.classList.remove("dark");
+          document.documentElement.classList.add("light");
+        } else {
+          document.documentElement.classList.remove("light");
+          document.documentElement.classList.add("dark");
         }
       }
     }
-    // handleChange();
+    handleChange()
+
     mediaQuery.addEventListener("change", handleChange);
 
     return () => mediaQuery.removeEventListener("change", handleChange);
   }, []);
 
   useEffect(() => {
-    if (mode === "dark") {
-      window.localStorage.setItem("theme", "dark");
-      document.documentElement.classList.add("dark");
-    }
+ if(mode==="dark"){
+  window.localStorage.setItem("theme","dark")
+  document.documentElement.classList.add("dark"); 
+  document.documentElement.classList.remove("light"); 
+}
+else {
+  window.localStorage.setItem("theme","light")
+  document.documentElement.classList.add("light"); 
+  document.documentElement.classList.remove("dark"); 
 
-    if (mode === "light") {
-      window.localStorage.setItem("theme", "light");
+}
 
-      document.documentElement.classList.remove("dark");
-    }
   }, [mode]);
-  return [mode, setMode] as const;
+  return [mode,setMode] as const;
 }
